110cb201ff6b19b04a5ce4dda68df03af063325b
Alex Charles <alexscharles@gmail.com>
2014-03-01 15:47:36 -0600

    didn't update before editing

--- RogueTest/web/position.dart
+++ RogueTest/web/position.dart
@@ -3,11 +3,9 @@ library position;
 class Position {
   int x;
   int y;
-  
+
   Position(this.x, this.y) {}
-  
-  Position DistanceTo(Position p) {
-    return new Position(this.x - p.x, this.y - p.y);
-  }
-  
+
+  Position DistanceTo(Position p) => new Position(this.x - p.x, this.y - p.y);
+
 }
